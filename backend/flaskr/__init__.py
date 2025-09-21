30cebce375734f6762d0e8ca9b4dbd2e3b571b6c
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-09-21 20:41:39 -0300

    fix cors to run delete

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -25,13 +25,10 @@ def create_app(config_object='config'):
     @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     """
     CORS(app,
-         origins=['http://localhost:5173'],
-         methods=['GET','POST'],
+         origins=['http://localhost:5173','http://127.0.0.1:5173'],
          supports_credentials=True   
-         )
-    
-    #CASO NECESSÁRIO EM ALGUMA ROTA EM ESPECIAL USAR
-    #@cross_origin()
+         #allow_headers=['Content-Type','Authorization'] apenas se forem enviados esses headers no fetch.
+    )
 
     #before requests:
     @app.before_request
@@ -52,6 +49,35 @@ def create_app(config_object='config'):
         q = row.question
         return f'ok: {q}'
     
+    @app.route('/addAllQuestions')
+    def addQuestions():
+        questions = [
+            Question(question="Whose autobiography is entitled 'I Know Why the Caged Bird Sings'?", answer="Maya Angelou",              difficulty=2, category=4),
+            Question(question="What boxer's original name is Cassius Clay?",                         answer="Muhammad Ali",              difficulty=1, category=4),
+            Question(question="What movie earned Tom Hanks his third straight Oscar nomination, in 1996?", answer="Apollo 13",        difficulty=4, category=5),
+            Question(question="What actor did author Anne Rice first denounce, then praise in the role of her beloved Lestat?", answer="Tom Cruise", difficulty=4, category=5),
+            Question(question="What was the title of the 1990 fantasy directed by Tim Burton about a young man with multi-bladed appendages?", answer="Edward Scissorhands", difficulty=3, category=5),
+            Question(question="Which is the only team to play in every soccer World Cup tournament?", answer="Brazil",                   difficulty=3, category=6),
+            Question(question="Which country won the first ever soccer World Cup in 1930?",           answer="Uruguay",                  difficulty=4, category=6),
+            Question(question="Who invented Peanut Butter?",                                          answer="George Washington Carver",  difficulty=2, category=4),
+            Question(question="What is the largest lake in Africa?",                                  answer="Lake Victoria",             difficulty=2, category=3),
+            Question(question="In which royal palace would you find the Hall of Mirrors?",            answer="The Palace of Versailles",  difficulty=3, category=3),
+            Question(question="The Taj Mahal is located in which Indian city?",                       answer="Agra",                      difficulty=2, category=3),
+            Question(question="Which Dutch graphic artist–initials M C was a creator of optical illusions?", answer="Escher",          difficulty=1, category=2),
+            Question(question="La Giaconda is better known as what?",                                 answer="Mona Lisa",                 difficulty=3, category=2),
+            Question(question="How many paintings did Van Gogh sell in his lifetime?",                answer="One",                       difficulty=4, category=2),
+            Question(question="Which American artist was a pioneer of Abstract Expressionism, and a leading exponent of action painting?", answer="Jackson Pollock", difficulty=2, category=2),
+            Question(question="What is the heaviest organ in the human body?",                        answer="The Liver",                 difficulty=4, category=1),
+            Question(question="Who discovered penicillin?",                                           answer="Alexander Fleming",         difficulty=3, category=1),
+            Question(question="Hematology is a branch of medicine involving the study of what?",      answer="Blood",                     difficulty=4, category=1),
+            Question(question="Which dung beetle was worshipped by the ancient Egyptians?",           answer="Scarab",                    difficulty=4, category=4),
+        ]
+
+        for q in questions:
+            q.insert()
+
+        return 'Questions inserted!'
+
     """
     @TODO:
     done
@@ -137,8 +163,6 @@ def create_app(config_object='config'):
             'total_questions':len(questions),
             'current_category':id
         }
-        
-    
 
     """
     
@@ -149,9 +173,9 @@ def create_app(config_object='config'):
     This removal will persist in the database and when you refresh the page.
     """
     @app.route('/questions/<int:id>',methods=['DELETE'])
-    @cross_origin()
     def delete_questions(id):
         try:
+            print(f"O id recebido foi o {id}")
             question = db.session.get(Question,id)
             if not question:
                 abort(404, description=f"O id: {id} não foi encontrado no db")
